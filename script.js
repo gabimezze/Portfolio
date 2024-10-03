791d4906a158fcc2419ebd4d45f8911070b0c32c
gabimezze <gabrielagomescezar@gmail.com>
2024-10-03 15:13:14 -0300

    add menu

---
+++ script.js
@@ -0,0 +1,7 @@
+// MENU DE LINKS
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    var overlay = document.getElementById('overlay');
+    menu.classList.toggle('open');
+    overlay.classList.toggle('show');
+}
