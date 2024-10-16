f8e7afa98a8210a159b99474d655a3eac53e7f11
gabimezze <gabrielagomescezar@gmail.com>
2024-10-16 09:50:53 -0300

    update rodape

--- my-app/src/components/Rodape/Rodape.tsx
+++ my-app/src/components/Rodape/Rodape.tsx
@@ -1,8 +1,7 @@
 
 export default function Rodape() {
     return (
-        <footer className="rodape">
-            <hr />
+        <footer className="bg-transparent text-white container mx-auto flex justify-center mt-24 mb-0">
             <p>&copy; 2024 Gabriela Cezar. Todos os direitos reservados.</p>
         </footer>
     )
